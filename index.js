e4f7cd92234fb966d863b7754af830663610e36f
rejaul360 <rejaraju2k22@gmail.com>
2023-05-20 18:35:41 +0600

    update toy infomartions

--- index.js
+++ index.js
@@ -5,12 +5,34 @@ require('dotenv').config()
 
 const port =  process.env.PORT || 5000 ;
 
-app.use(cors())
+const corsConfig = {
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig))
+app.options("", cors(corsConfig))
+
+// app.use(function(req, res, next) {
+//   // Set the allowed origin to "*" to allow requests from any origin
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+
+//   // Additional headers to allow (optional)
+//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+//   // Set the allowed methods (optional)
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+
+//   next();
+// });
+
+
+
 app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.arwkaoj.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,7 +47,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const toyCollections = client.db('toyMarket').collection('toys')
 
@@ -63,16 +85,44 @@ async function run() {
         res.send(result)
     })
 
-    app.get('/myToy/:email', async(req,res)=>{
-      console.log(req.params.email)
-      const result = await toyCollections.find({postedBy:req.params.email}).toArray()
-      res.send(result);
+    app.get('/allToy/:id', async(req,res)=>{
+      const id =req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await toyCollections.findOne(query)
+      res.send(result)
     })
+
+    //delete operationss-------------------
+    app.delete('/allToy/:id', async(req,res)=>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await toyCollections.deleteOne(query)
+      res.send(result)
+    })
+
+    // app.get('/myToy/:email', async(req,res)=>{
+    //   console.log(req.params.email)
+    //   const result = await toyCollections.find({postedBy:req.params.email}).toArray()
+    //   res.send(result);
+    // })
+
+
+    // app.put("/updateInfo/:id", async(req,res)=>{
+    //   const id =req.params.id
+    //   console.log(id);
+    //   const body = req.body
+    //   console.log(body);
+
+    //   const filter = { _id: new ObjectId(id)}
+    //   console.log(filter);
+
+
+    // })
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
